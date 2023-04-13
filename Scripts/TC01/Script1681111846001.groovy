ae1536e48e62bba9ee146bf4bb1b1c3938e4cf00
ptannguyen <ptannguyen@hitachivantara.com>
2023-04-13 09:29:33 +0700

    update open Browser

--- Scripts/TC01/Script1681111846001.groovy
+++ Scripts/TC01/Script1681111846001.groovy
@@ -18,9 +18,9 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+WebUI.openBrowser('https://katalon-demo-cura.herokuapp.com/')
 
-WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
+//WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
 
 WebUI.maximizeWindow()
 
